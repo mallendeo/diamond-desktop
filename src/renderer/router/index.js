ab38cd8d1666dbe524ef703361168fcd7673b886
Mauricio Allende <mauricio.allende.o@gmail.com>
2018-03-23 20:14:55 -0300

    add routes, remove unused reducer

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -1,17 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Home from '@/pages/home'
 import LandingPage from '@/pages/landing-page'
 import GettingStarted from '@/pages/getting-started'
 import RestoreBackup from '@/pages/restore-backup'
+import CreatePortfolio from '@/pages/create-portfolio'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      name: 'home',
       path: '/',
+      component: Home,
+      children: [
+        {
+          path: 'create',
+          component: CreatePortfolio
+        }
+      ]
+    },
+    {
       name: 'landing-page',
+      path: '/start',
       component: LandingPage
     },
     {
